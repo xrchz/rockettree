f58a2e2c92f3c5fd8b411434ba5ca537ca84301e
Ramana Kumar <ramana@member.fsf.org>
2023-06-10 18:45:38 +0100

    Start implementation

---
+++ run.js
@@ -0,0 +1,62 @@
+import 'dotenv/config'
+import { ethers } from 'ethers'
+
+const rocketStorageAddresses = new Map()
+rocketStorageAddresses.set('mainnet', '0x1d8f8f00cfa6758d7bE78336684788Fb0ee0Fa46')
+rocketStorageAddresses.set('goerli', '0xd8Cd47263414aFEca62d6e2a3917d6600abDceB3')
+
+const genesisTimes = new Map()
+genesisTimes.set('mainnet', 1606824023)
+genesisTimes.set('goerli', 1616508000)
+
+const beaconRpcUrl = process.env.BN_URL || 'http://localhost:5052'
+const provider = new ethers.JsonRpcProvider(process.env.RPC_URL || 'http://localhost:8545')
+const networkName = await provider.getNetwork().then(n => n.name)
+const rocketStorage = new ethers.Contract(
+  rocketStorageAddresses.get(networkName),
+  ['function getAddress(bytes32) view returns (address)'], provider)
+console.log(`Using Rocket Storage ${await rocketStorage.getAddress()}`)
+
+const rocketRewardsPool = new ethers.Contract(
+  await rocketStorage['getAddress(bytes32)'](ethers.id('contract.addressrocketRewardsPool')),
+  ['function getClaimIntervalTimeStart() view returns (uint256)',
+   'function getClaimIntervalTime() view returns (uint256)'
+  ],
+  provider)
+
+const startTime = await rocketRewardsPool.getClaimIntervalTimeStart()
+console.log(`startTime: ${startTime}`)
+
+const intervalTime = await rocketRewardsPool.getClaimIntervalTime()
+console.log(`intervalTime: ${intervalTime}`)
+
+const latestBlockTime = await provider.getBlock('latest').then(b => b.timestamp)
+console.log(`latestBlockTime: ${latestBlockTime}`)
+
+const timeSinceStart = BigInt(latestBlockTime) - BigInt(startTime)
+const intervalsPassed = timeSinceStart / intervalTime
+console.log(`intervalsPassed: ${intervalsPassed}`)
+
+const endTime = startTime + (intervalTime * intervalsPassed)
+console.log(`endTime: ${endTime}`)
+
+const genesisTime = BigInt(genesisTimes.get(networkName))
+const secondsPerSlot = 12n
+const slotsPerEpoch = 32n
+
+const totalTimespan = endTime - genesisTime
+
+let targetBcSlot = totalTimespan / secondsPerSlot
+if (totalTimespan % secondsPerSlot) targetBcSlot++
+async function checkSlotExists(slotNumber) {
+  const url = new URL(`/eth/v1/beacon/headers/${slotNumber}`, beaconRpcUrl)
+  const response = await fetch(url)
+  if (response.status !== 200 && response.status !== 404)
+    console.warn(`Unexpected response status getting ${slotNumber} header: ${response.status}`)
+  return response.status === 200
+}
+while (!(await checkSlotExists(targetBcSlot))) targetBcSlot--
+
+const targetSlotEpoch = targetBcSlot / slotsPerEpoch
+console.log(`targetBcSlot: ${targetBcSlot}`)
+console.log(`targetSlotEpoch: ${targetSlotEpoch}`)
