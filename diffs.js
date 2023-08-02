9c048686211e3d30bfd4c4cbae99f6c4597da925
Ramana Kumar <ramana@member.fsf.org>
2023-08-02 19:14:19 +0100

    Use log instead of warn in diffs (for ease with less)

--- diffs.js
+++ diffs.js
@@ -1,21 +1,23 @@
 import { readFileSync } from 'node:fs'
-const official = JSON.parse(readFileSync('/home/ramana/Downloads/rp-rewards-mainnet-11.json', {encoding: 'utf8'})).nodeRewards
+const official = JSON.parse(readFileSync('/home/ramana/Downloads/rp-rewards-mainnet-12-partial.json', {encoding: 'utf8'})).nodeRewards
 const mine = JSON.parse(readFileSync('node-rewards.json', {encoding: 'utf8'}))
 const mykeys = Object.keys(mine).map(a => a.toLowerCase())
 const officialkeys = Object.keys(official)
 for (const key of officialkeys) {
-  if (!mykeys.includes(key))
-    console.warn(`${key} in official but not mine`)
+  const {smoothingPoolEth, collateralRpl, oracleDaoRpl} = official[key]
+  const offvals = [smoothingPoolEth, collateralRpl, oracleDaoRpl].join()
+  if (!mykeys.includes(key) && offvals != '0,0,0')
+    console.log(`${key} in official ${offvals} but not mine`)
 }
 for (const key of mykeys) {
   if (!officialkeys.includes(key))
-    console.warn(`${key} in mine but not official`)
+    console.log(`${key} in mine but not official`)
 }
 for (const [node, {ETH, RPL}] of Object.entries(mine)) {
   const {smoothingPoolEth, collateralRpl, oracleDaoRpl} = official[node.toLowerCase()]
   const officialRPL = BigInt(collateralRpl) + BigInt(oracleDaoRpl)
   if (ETH != smoothingPoolEth)
-    console.warn(`${node} ETH discrepancy: ${smoothingPoolEth} vs ${ETH} (diff ${BigInt(smoothingPoolEth) - BigInt(ETH)})`)
+    console.log(`${node} ETH discrepancy: ${smoothingPoolEth} vs ${ETH} (diff ${BigInt(smoothingPoolEth) - BigInt(ETH)})`)
   if (RPL != officialRPL)
-    console.warn(`${node} RPL discrepancy: ${officialRPL} vs ${RPL}`)
+    console.log(`${node} RPL discrepancy: ${officialRPL} vs ${RPL}`)
 }
