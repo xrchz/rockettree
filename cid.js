a2ab29104e1f208c93765406fadc0af9a9c94fe9
Ramana Kumar <ramana@member.fsf.org>
2024-05-04 16:42:33 +0100

    Add cid generator

---
+++ cid.js
@@ -0,0 +1,17 @@
+import { importer } from 'ipfs-unixfs-importer'
+import { MemoryBlockstore } from 'blockstore-core'
+import { readFileSync } from 'node:fs'
+
+const sources = [
+  {path: process.env.FILENAME, content: readFileSync(process.env.FILENAME)}
+]
+const options = {
+  wrapWithDirectory: true,
+  reduceSingleLeafToSelf: true,
+  shardSplitThresholdBytes: 4 * 1024 * 1024,
+  rawLeaves: true,
+  cidVersion: 1
+}
+const blockstore = new MemoryBlockstore()
+for await (const {cid, path} of importer(sources, blockstore, options))
+  if (!path) console.log(cid.toString())
